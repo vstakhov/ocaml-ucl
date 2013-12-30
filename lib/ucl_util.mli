972efcef3cbf30779340e95c4bad77d0c5ac9cee
Vsevolod Stakhov <vsevolod@highsecure.ru>
2013-12-30 16:52:17 +0000

    Hide private interfaces.

--- lib/ucl_util.mli
+++ lib/ucl_util.mli
@@ -1,22 +1,5 @@
 val unwind : protect:('a -> unit) -> ('a -> 'b) -> 'a -> 'b
 val with_input_channel : in_channel -> (in_channel -> 'a) -> 'a
-val make_cset : string -> bool array
-val in_cset : 'a array -> char -> 'a
-val alpha : bool array
-val alnum : bool array
-val blank : bool array
-val cntrl : bool array
-val digit : bool array
-val graph : bool array
-val lower : bool array
-val upper : bool array
-val punct : bool array
-val print : bool array
-val space_unsafe : bool array
-val space_safe : bool array
-val xdigit : bool array
-val keystart : bool array
-val valueend : bool array
 val isalpha : char -> bool
 val isalnum : char -> bool
 val isblank : char -> bool
